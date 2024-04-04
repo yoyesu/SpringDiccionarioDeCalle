91a8cc71fc8705ca1e13f82efed8d6168c457960
yoyesu <yoyes15@gmail.com>
2024-04-04 19:58:48 +0100

    [DDC-16] simplifying entry model

--- src/main/java/com/mdlsf/springdiccionariodecalle/security/MyUserDetailsService.java
+++ src/main/java/com/mdlsf/springdiccionariodecalle/security/MyUserDetailsService.java
@@ -1,25 +1,23 @@
-package com.mdlsf.springdiccionariodecalle.security;
-
-import com.mdlsf.springdiccionariodecalle.entities.User;
-import com.mdlsf.springdiccionariodecalle.repos.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
-
-@Service
-public class MyUserDetailsService implements UserDetailsService {
-
-    @Autowired
-    private UserRepository userRepository;
-
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByUserName(username);
-        if (user == null) {
-            throw new UsernameNotFoundException("User not found");
-        }
-        return new MyUserPrincipal(user);
-    }
-}
+//package com.mdlsf.springdiccionariodecalle.security;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class MyUserDetailsService implements UserDetailsService {
+//
+//    @Autowired
+//    private UserRepository userRepository;
+//
+//    @Override
+//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+//        User user = userRepository.findByUserName(username);
+//        if (user == null) {
+//            throw new UsernameNotFoundException("User not found");
+//        }
+//        return new MyUserPrincipal(user);
+//    }
+//}
--- src/main/java/com/mdlsf/springdiccionariodecalle/security/UserDetailsConfig.java
+++ src/main/java/com/mdlsf/springdiccionariodecalle/security/UserDetailsConfig.java
@@ -1,22 +1,22 @@
-package com.mdlsf.springdiccionariodecalle.security;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-@Configuration
-public class UserDetailsConfig {
-
-    @Bean
-    public UserDetailsService userDetailsService() {
-        return new MyUserDetailsService();
-    }
-
-    @Bean
-    public static PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-}
-
+//package com.mdlsf.springdiccionariodecalle.security;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//
+//@Configuration
+//public class UserDetailsConfig {
+//
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        return new MyUserDetailsService();
+//    }
+//
+//    @Bean
+//    public static PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//}
+//
