3e235ab56557e8d5f2bee1649893ea8af39f1243
Maria Lopez <99568298+yoyesu@users.noreply.github.com>
2024-04-13 00:25:26 +0100

    [DDC-17] Integrating with postgres database (#4)

--- src/main/java/com/mdlsf/springdiccionariodecalle/entities/Entry.java
+++ src/main/java/com/mdlsf/springdiccionariodecalle/entities/Entry.java
@@ -1,5 +1,6 @@
 package com.mdlsf.springdiccionariodecalle.entities;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -17,26 +18,32 @@ import java.util.Set;
 @Entity
 @Table(name = "entries")
 public class Entry {
+    @JsonProperty
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
     private Integer id;
 
+    @JsonProperty
     @Column(name = "term", nullable = false)
     private String term;
 
+    @JsonProperty
     @Column(name = "def", nullable = false)
     private String def;
 
+    @JsonProperty
     @NotNull
     @Column(name = "last_updated", nullable = false)
     @Builder.Default
     private Instant lastUpdated = Instant.now();
 
+    @JsonProperty
     @NotNull
     @Column(name = "date_added", nullable = false)
     @Builder.Default
     private Instant dateAdded = Instant.now();
 
+    @JsonProperty
     @Builder.Default
     private String userAdded = "admin";
     private Set<String> countryUse;
